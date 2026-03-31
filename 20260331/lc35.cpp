ffaa515a1231a3db16b0de76502b8779fbe3eea9
siyuancheng <syuan.cheng21@gmail.com>
2026-03-31 09:21:14 +0800

    二分查找

---
+++ 20260331/lc35.cpp
@@ -0,0 +1,23 @@
+#include <cstddef>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int searchInsert(vector<int> &nums, int target) {
+    int n = nums.size();
+    int l = 0, r = n - 1;
+    while (l <= r) {
+      int mid = l + ((r - l) >> 1);
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] < target) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    }
+    return l;
+  }
+};
