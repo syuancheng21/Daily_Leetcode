37f443b8558301e2badec93f4b33370dab1e62c2
siyuancheng <syuan.cheng21@gmail.com>
2026-04-13 17:23:41 +0800

    update lc143

--- link_list/lc143.cpp
+++ link_list/lc143.cpp
@@ -1,10 +1,19 @@
-struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
+#include "list_node.h"
+
+/**
+143. 重排链表
+给定一个单链表 L 的头节点 head ，单链表 L 表示为：
+L0 → L1 → … → Ln - 1 → Ln
+请将其重新排列后变为：
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+示例 1：
+输入：head = [1,2,3,4]
+输出：[1,4,2,3]
+示例 2：
+输入：head = [1,2,3,4,5]
+输出：[1,5,2,4,3]
+*/
 
 class Solution {
 public:
