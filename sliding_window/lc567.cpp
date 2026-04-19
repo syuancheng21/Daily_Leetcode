c1bebe64a83e2a359287cfd108a87b3cab732b39
siyuancheng <syuan.cheng21@gmail.com>
2026-04-19 20:39:11 +0800

    update 567

--- sliding_window/lc567.cpp
+++ sliding_window/lc567.cpp
@@ -11,27 +11,46 @@ class Solution {
 public:
   bool checkInclusion(string s1, string s2) {
     int l1 = s1.size(), l2 = s2.size();
-    if (l2 < l1) {
+    if (l2 < l1) { // s2 include s1
       return false;
     }
 
-    vector<int> s1_count(26);
-    vector<int> s2_count(26);
+    int count[26] = {0};
 
     for (int i = 0; i < l1; i++) {
-      s1_count[s1[i] - 'a']++;
-      s2_count[s2[i] - 'a']++;
+      count[s2[i] - 'a']++;
+      count[s1[i] - 'a']--;
     }
 
-    if (s1_count == s2_count) {
+    int diff = 0;
+    for (int i = 0; i < 26; i++) {
+      if (count[i] != 0) {
+        diff++;
+      }
+    }
+
+    if (diff == 0) {
       return true;
     }
 
     for (int i = 0; i < l2 - l1; i++) {
-      s2_count[s2[i] - 'a']--;
-      s2_count[s2[i + l1] - 'a']++;
+      int l = s2[i] - 'a';
+      if (count[l] == 1) {
+        diff--;
+      } else if (count[l] == 0) {
+        diff++;
+      }
+      count[l]--;
 
-      if (s1_count == s2_count) {
+      int r = s2[i + l1] - 'a';
+      if (count[r] == -1) {
+        diff--;
+      } else if (count[r] == 0) {
+        diff++;
+      }
+      count[r]++;
+
+      if (diff == 0) {
         return true;
       }
     }
