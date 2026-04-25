8d121cbe7e2c4a76bed5b75e2a29ae95aca266c9
siyuancheng <syuan.cheng21@gmail.com>
2026-04-25 20:44:46 +0800

    环形指针

---
+++ basics/lc141_linked_list_cycle.cpp
@@ -0,0 +1,21 @@
+#include "type.h"
+
+
+class Solution {
+public:
+  bool hasCycle(ListNode *head) {
+    if (head == nullptr || head->next == nullptr) {
+      return false;
+    }
+    ListNode *slow = head, *fast = head;
+    while (fast != nullptr && fast->next != nullptr) {
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+};
